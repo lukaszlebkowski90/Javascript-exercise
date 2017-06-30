613fea2b3ebe59ee5c1ae8ae92d22d80b398374a
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-06-30 13:17:24 +0200

    exercise_4

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+// Zadanie: Pole trójkąta
+// Zadeklaruj zmienne a, h oraz triangleArea. Nadaj im wartość początkową dla a oraz h (jakieś liczby).
+// Przypisz do triangleArea działanie a*h/2.
+// Na koniec wypisz console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+// Do pobrania danych możesz także skorzystać z funkcji prompt.
+// Wyślij zmiany na nowe repozytorium na GitHubie. Pamiętaj o udostępnieniu go mentorowi na GitHubie. Link do projektu wstaw poniżej.
 
-/*  O ten komentarz chodzi :) 
-    ???????
-    ??????
-*/
 
-var age = prompt("How old are you?");
-alert(age + "!?! really ? you are to old!");
-console.log(age + " !?! really ? you are to old!");
+var a = prompt("Podaj liczbe");
+var h = prompt("Podaj liczbe");
+var triangleArea = (a*h)/2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
