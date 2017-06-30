7308133d958d3d4523e02a16278ab2636c427dfc
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-06-30 13:09:08 +0200

    exercise_3

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+/*  O ten komentarz chodzi :) 
+    ???????
+    ??????
+*/
+
+var age = prompt("How old are you?");
+alert(age + "!?! really ? you are to old!");
+console.log(age + " !?! really ? you are to old!");
