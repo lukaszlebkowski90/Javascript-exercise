db71399328c87a279130a0475fe14cc69110bfc1
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-06-30 13:31:52 +0200

    exercise_5

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
-// Zadanie: Pole trójkąta
-// Zadeklaruj zmienne a, h oraz triangleArea. Nadaj im wartość początkową dla a oraz h (jakieś liczby).
-// Przypisz do triangleArea działanie a*h/2.
-// Na koniec wypisz console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-// Do pobrania danych możesz także skorzystać z funkcji prompt.
-// Wyślij zmiany na nowe repozytorium na GitHubie. Pamiętaj o udostępnieniu go mentorowi na GitHubie. Link do projektu wstaw poniżej.
+// Zadanie: Instrukcje warunkowe w praktyce
+// Stwórz zmienne potrzebne do wykonania działania (a * a) + (2 * a * b) - (b * b) , a jego wynik przypisz do zmiennej value. Wyświetl wynik w konsoli, a następnie za pomocą instrukcji warunkowej sprawdź, czy wynik jest dodatni czy ujemny, a potem wyświetl informację wynik dodatni lub wynik ujemny w konsoli.
+// Dodaj instrukcję, która sprawdzi, czy wynik jest równy zero i wyświetli tę informację.
+//  Stwórz nowe repozytorium i wyślij tam swoje zadanie, a link do niego wklej poniżej. Pamiętaj o udostępnieniu repozytorium swojemu mentorowi.
 
 
-var a = prompt("Podaj liczbe");
-var h = prompt("Podaj liczbe");
-var triangleArea = (a*h)/2;
+var a = prompt("wpisz liczbe");
+var b = prompt("wpisz liczbe");
 
-alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+if ((a * a) + (2 * a * b) - (b * b)>0) {
+    alert("Liczba jest dodatnia");
+    console.log("Liczba jest dodatnia");
+} else if ((a * a) + (2 * a * b) - (b * b)<0) {
+    alert("Liczba jest ujemna");
+    console.log("Liczba jest ujemna");
+} else {
+    alert("Wynik = 0");
+    console.log("Wynik = 0");
+}
