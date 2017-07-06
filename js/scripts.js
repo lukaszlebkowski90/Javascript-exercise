44e97af8abd22d0e7d5b1c44c1e62b8d817c9a63
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-07-06 11:53:31 +0200

    order in function

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,10 @@ var add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
 	var element = document.createElement('li');
+  var order = list.getElementsByTagName('li');
   element.innerHTML = 'item '+ order.length;
   element.getElementsByTagName('li');
   list.appendChild(element);
 });
-var order = list.getElementsByTagName('li');
+
 
