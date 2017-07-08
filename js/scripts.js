9a4637b560316945177b9587d94f386490caf715
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-07-08 19:16:18 +0200

    update exercise_11

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ var add = document.getElementById('addElem');
 add.addEventListener('click', function() {
 	var element = document.createElement('li');
   var order = list.getElementsByTagName('li');
-  element.innerHTML = 'item '+ order.length;
   element.getElementsByTagName('li');
+  element.innerHTML = 'item '+ order.length;
   list.appendChild(element);
 });
 
