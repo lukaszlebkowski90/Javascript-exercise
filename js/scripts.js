7c8e117af85c8f14af1db9f851213e088ac41683
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-07-08 16:26:50 +0200

    update exercise_7

--- js/scripts.js
+++ js/scripts.js
@@ -22,14 +22,10 @@
 
 // Stwórz nowe repozytorium i wyślij tam swoje zadanie, a link do niego wklej poniżej. Pamiętaj o udostępnieniu repozytorium swojemu mentorowi.
 
-var femaleNames = ['Mariola', 'Jolasia', 'Halina', 'Helena'];
-var maleNames = ['Ferdek', 'Pazdzioch', 'Waldus', 'Boczek'];
-var all = [];
-var allNames = all.concat(femaleNames,maleNames);
-console.log(allNames);
-var newName = "menda";
-	if ( allNames.indexOf(newName)  ===  -1) {
-  	  allNames.push(newName);
-  } else {
-  };
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+var newName = "Marian";
+	 if (allNames.indexOf(newName) === -1) 
+  			allNames.push(newName);
 console.log(allNames);
