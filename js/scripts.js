0b5e9e41017f0526a524aab79c5f04a295f67b82
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-07-09 15:10:16 +0200

    update exercise_6

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function getTriangleArea(a, h) {
 	if (a > 0 && h > 0) 
 		return a * h / 2;
+		return "niewłaściwe dane";
 }
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(2, 15);
